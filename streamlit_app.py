eb8b1f1079bd708f5cecb7ff8c24c5c042a0dcfb
nlee3834 <140723255+nlee3834@users.noreply.github.com>
2023-09-13 14:34:27 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,9 +25,9 @@ st.dataframe(fruits_to_show)
 
 # New Section to display fruityvice api response
 st.header('Fruityvice Fruit Advice!')
-fruit_choice = st.text_input('What fruit would you like information about?','Kiwi')
+fruit_choice = st.text_input('What fruit would you like information about?', 'Kiwi')
 # User can choose fruit
-st.write('The user entered ', fruit_choice)
+st.write('The user entered', fruit_choice)
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
